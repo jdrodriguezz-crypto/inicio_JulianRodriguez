9ab84542951dc244b0e212647c56dd84eb545904
julia <julia@PC-JULIAN>
2026-04-08 13:36:26 +0200

    cambio de idioma

--- src/practicagitHub/GitHub.java
+++ src/practicagitHub/GitHub.java
@@ -5,7 +5,7 @@ public class GitHub {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola buenas, soy julian y mi compañera?");
-		System.out.println("Hola Julián, soy Karina tu compañera. ¿What's up?");
+		System.out.println("Hola Julián, soy Karina tu compañera. ¿QUE PASAAAAAA!!!!!!?");
 
 	}
 
