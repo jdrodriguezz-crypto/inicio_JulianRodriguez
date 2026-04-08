dba602a877f43e954db671ba9749b0515545d426
julia <julia@PC-JULIAN>
2026-04-08 12:55:57 +0200

    primer mensaje

--- src/practicagitHub/GitHub.java
+++ src/practicagitHub/GitHub.java
@@ -3,7 +3,8 @@ package practicagitHub;
 public class GitHub {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hola buenas, soy julian y mi compañera?");
 
 	}
 
